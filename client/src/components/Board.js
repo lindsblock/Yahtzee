68fa8f51879e6d1ee07a3a8af34e85a735a87a81
Lindsay Block <lindsay.block05@gmail.com>
2018-03-27 14:11:44 -0600

    initial commit

---
+++ client/src/components/Board.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+  Grid,
+  Button,
+  Divider,
+} from 'semantic-ui-react'
+import Dice from './Dice'
+import { connect } from 'react-redux';
+import { rollDice } from '../actions/currentGame';
+
+const Board = ({
+  roll,
+  dice,
+  keep,
+  dispatch
+}) => {
+  const maxRoll = roll === 3;
+  const disabled = maxRoll ? { disabled: true } : {}
+  return (
+    <Grid>
+      <Grid.Row>
+        <Button
+          fluid
+          onClick={() => dispatch(rollDice())}
+          {...disabled}
+        >
+          { maxRoll ? 'Score Roll' : 'Roll Dice' }
+        </Button>
+        <Grid.Column width={16}>
+          <Divider hidden />
+        </Grid.Column>
+        { roll > 0 &&
+            dice.map( (d,i) => {
+              const kept = keep.includes(i)
+              return (
+                <Dice
+                  key={i}
+                  value={d}
+                  kept={kept}
+                  index={i}
+                />
+              )
+            })
+          }
+      </Grid.Row>
+    </Grid>
+  )
+}
+const mapStateToProps = (state) => {
+  const { roll, dice, keep } = state.currentGame;
+  return{ roll, dice, keep }
+}
+export default connect(mapStateToProps)(Board);
